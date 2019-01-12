d8e00b899556bc3d084417ae5f74f7f5b30e9765
anshu247 <46483372+anshu247@users.noreply.github.com>
2019-01-12 23:15:09 +0530

    Update Series.java

--- Series.java
+++ Series.java
@@ -5,6 +5,10 @@ public static void main(String args[])
 {
 int terms,count,total=0;
 double x ;
+char choice;
+Console console = System.console();
+	do 
+	{    
 Scanner as=new Scanner(System.in);
 
 System.out.println("enter the value of X");
@@ -18,5 +22,9 @@ for(count=1;count<=terms;count++)
     total+=Math.pow(x,count)/count;
 }
 System.out.println("the SUM is"+total);
+        choice = console.readLine("Do You Want To Continue (Y/N)?: ").charAt(0);
+         
+        }while((choice!='n')&&(choice!='N'));
+
 }
 }
