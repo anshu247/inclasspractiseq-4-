b8cb11a5a30de0459dce4328399c80104d39d63c
anshu247 <46483372+anshu247@users.noreply.github.com>
2019-01-12 23:37:52 +0530

    exercise question of control statment and data types

---
+++ Oper.java
@@ -0,0 +1,21 @@
+class Oper{
+public static void main(String args[]);
+{
+	int a=9;
+	int b=7;
+	int sum=a+b;
+	int mul=a*b;
+	int div=a/b;
+	int sub=a-b;
+	int mod=a%b;
+
+	System.out.println("sum of a"+a+"and b"+b+" is equal to:"+sum);
+	System.out.println("multipication of a"+a+"and b"+b+" is equal to:"+mul);
+	System.out.println("modulus of a"+a+"and b"+b+" is equal to:"+mod);
+	System.out.println("divison  of a"+a+"and b"+b+" is equal to:"+div);
+	System.out.println("subtration of a"+a+"and b"+b+" is equal to:"+sub);
+	
+}
+
+
+}
---
+++ Withdatatype.java
@@ -0,0 +1,65 @@
+class Withdatatype{
+public static void main(String args[]);
+{
+	double a=9.09439454;
+	double b=7.65498574;
+	double sum=a+b;
+	double mul=a*b;
+	double div=a/b;
+	double sub=a-b;
+	double mod=a%b;
+
+	System.out.println("sum of a"+a+"and b"+b+" is equal to:"+sum);
+	System.out.println("multipication of a"+a+"and b"+b+" is equal to:"+mul);
+	System.out.println("modulus of a"+a+"and b"+b+" is equal to:"+mod);
+	System.out.println("divison  of a"+a+"and b"+b+" is equal to:"+div);
+	System.out.println("subtration of a"+a+"and b"+b+" is equal to:"+sub);
+
+	int a=9.09439454;
+	int b=7.65498574;
+	int sum1=a+b;
+	int mul1=a*b;
+	int div1=a/b;
+	int sub1=a-b;
+	int mod1=a%b;
+
+	System.out.println("sum of a"+a+"and b"+b+" is equal to:"+sum1);
+	System.out.println("multipication of a"+a+"and b"+b+" is equal to:"+mul1);
+	System.out.println("modulus of a"+a+"and b"+b+" is equal to:"+mod1);
+	System.out.println("divison  of a"+a+"and b"+b+" is equal to:"+div1);
+	System.out.println("subtration of a"+a+"and b"+b+" is equal to:"+sub1);
+	
+	long  a=9.09439454;
+	long  b=7.65498574;
+	long sum3=a+b;
+	long mul3=a*b;
+    long div3=a/b;
+	long sub3=a-b;
+	long mod3=a%b;
+
+	System.out.println("sum of a"+a+"and b"+b+" is equal to:"+sum3);
+	System.out.println("multipication of a"+a+"and b"+b+" is equal to:"+mul3);
+	System.out.println("modulus of a"+a+"and b"+b+" is equal to:"+mod3);
+	System.out.println("divison  of a"+a+"and b"+b+" is equal to:"+div3);
+	System.out.println("subtration of a"+a+"and b"+b+" is equal to:"+sub3);
+	
+	float a=9.09439454;
+	float b=7.65498574;
+	float sum4=a+b;
+	float mul4=a*b;
+	float div4=a/b;
+	float sub4=a-b;
+	float mod4=a%b;
+
+	System.out.println("sum of a"+a+"and b"+b+" is equal to:"+sum4);
+	System.out.println("multipication of a"+a+"and b"+b+" is equal to:"+mul4);
+	System.out.println("modulus of a"+a+"and b"+b+" is equal to:"+mod4);
+	System.out.println("divison  of a"+a+"and b"+b+" is equal to:"+div4);
+	System.out.println("subtration of a"+a+"and b"+b+" is equal to:"+sub4);
+	
+	
+ 
+}
+
+
+}
