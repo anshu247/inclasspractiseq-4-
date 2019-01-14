453e69503dd519f19405773cccb451268987db07
anshu247 <46483372+anshu247@users.noreply.github.com>
2019-01-14 21:17:20 +0530

    ppt

---
+++ programfglb4.java
@@ -0,0 +1,12 @@
+
+class programfglb4{
+	public static void main(String args[]){
+		System.out.println("S.no\tData Types\tSize\tMinimum value\tMaximum value\t");
+		System.out.println("1\tlong\t64\t-9223372036854775808\t9223372036854775807");
+		System.out.println("2\tint\t32\t-2147483648\t\t2147483647");
+		System.out.println("3\tshort\t16\t-32768\t\t\t32767");
+		System.out.println("4\tbyte\t8\t-128\t\t\t127");
+		System.out.println("5\tdouble\t64\t4.9e-324\t\t\t1.8e+308");
+		System.out.println("6\tfloat\t32\t1.4e-045\t\t\t3.4e+038");
+	}
+}
